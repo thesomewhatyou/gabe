31fa931720dffdcf659f9a3cf804f3e61faaa54b
Gabriel <162917831+thesomewhatyou@users.noreply.github.com>
2025-11-12 21:58:41 +0000

    no.

--- commands/message/quote-message.js
+++ commands/message/quote-message.js
@@ -86,6 +86,7 @@ class QuoteMessageCommand extends Command {
       params: {
         text: decoratedQuote.replaceAll("&", "&amp;").replaceAll("<", "&lt;").replaceAll(">", "&gt;"),
         username: displayName.replaceAll("&", "&amp;").replaceAll("<", "&lt;").replaceAll(">", "&gt;"),
+        basePath: "/app/",
       },
       input: {
         data: avatarBuffer,
